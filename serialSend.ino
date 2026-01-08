2734c0814bc6412028e13a477cc70d06cc0b933b
hydrolisk1792 <48342347+hydrolisk1792@users.noreply.github.com>
2026-01-07 22:33:44 -0800

    Delete serialSend.ino

--- serialSend.ino
+++
@@ -1,8 +0,0 @@
-//Send Data:
-void serialSend(const int x)
-{
-  String sending = startDelimiter + String(x) + endDelimiter;
-  Serial1.print(String(sending));
-  Serial1.println();
-}
-//End Send Data
