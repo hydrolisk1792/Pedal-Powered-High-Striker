2986a3928f8717c3f572c7b520dd8ff554458c4c
hydrolisk1792 <48342347+hydrolisk1792@users.noreply.github.com>
2026-01-07 22:37:56 -0800

    Add files via upload

---
+++ ESP32/src/Button.h
@@ -0,0 +1,23 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <Arduino.h>
+
+class Button {
+public:
+  Button(uint8_t pin, unsigned long debounce = 50, bool activeLow = true);
+
+  bool pressed();       // returns true once per press
+  bool isDown();        // returns true as long as button is held
+  bool released();      // returns true once when released
+
+private:
+  uint8_t pin;
+  unsigned long lastChange = 0;
+  unsigned long debounceTime;
+  bool activeLow;
+
+  bool prevState = HIGH;
+};
+
+#endif
